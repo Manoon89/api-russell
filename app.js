882995e41e3cd73a7a4e91db6af52b248e7535f0
Manoon <manon.lagniel@gmail.com>
2025-03-12 15:48:28 +0100

    add mongoose & cors instructions

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const mongodb = require('./db/mongo');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+mongodb.initClientDbConnection();
+
+app.use(cors({
+  exposedHeaders: ['Authorization'],
+  origin: '*'
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,9 +32,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+/* use function learned in course instead
+catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
+}); */
+
+app.use(function(req, res, next){
+  res.status(404).json({name: 'API', version: '1.0', status: 404, message: 'not_found'});
 });
 
 // error handler
