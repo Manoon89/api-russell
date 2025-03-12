59197e9df6c5da39dea3dda073b0f8af675d96ba
Manoon <manon.lagniel@gmail.com>
2025-03-12 16:26:19 +0100

    Add authenticate

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const service = require('../services/users')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/authenticate', service.authenticate);
 
 module.exports = router;
