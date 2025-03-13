a90c8e4741691f16aebd372e47ef8ae41f8e04bf
Manoon <manon.lagniel@gmail.com>
2025-03-13 09:23:43 +0100

    complete private.js & add in routes/users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const private = require('../middlewares/private');
 
 const serviceUsers = require('../services/users')
 
 /* en commentaire en attendant de compléter les fichiers du dossier service
-router.get('/', serviceUsers.getById);
-router.get('/:email', serviceUsers.getByEmail);
+router.get('/', private.checkJWT, serviceUsers.getById);
+router.get('/:email', private.checkJWT, serviceUsers.getByEmail);
 router.post('/', serviceUsers.add);
-router.put('/:email', serviceUsers.update);
-router.delete('/:email', serviceUsers.delete);
+router.put('/:email', private.checkJWT, serviceUsers.update);
+router.delete('/:email', private.checkJWT, serviceUsers.delete);
 */
 
 /* importé avec express-generator. On garde en commentaire au cas où. 
